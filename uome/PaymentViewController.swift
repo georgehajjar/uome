aae80b106557e2050d2c4d6c5977de5ba094d237
George Hajjar <georgehajjar@George-Hajjars-Macbook-Pro.local>
2018-06-07 16:21:41 -0400

    Working on Add page

--- uome/PaymentViewController.swift
+++ uome/PaymentViewController.swift
@@ -12,7 +12,7 @@ class PaymentViewController: UIViewController, UIPickerViewDataSource, UIPickerV
 
     var temp = ""
     
-    
+    @IBOutlet weak var paymentTitle: UITextField!
     @IBOutlet weak var paidByPicker: UIPickerView!
     @IBOutlet weak var totalMoney: UITextField!
     
@@ -20,23 +20,41 @@ class PaymentViewController: UIViewController, UIPickerViewDataSource, UIPickerV
         super.viewDidLoad()
     }
     
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.paidByPicker.reloadAllComponents()
+    }
+    
+    //Set text color to white
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let str = DataManager.sharedManager.nameData[row]
+        return NSAttributedString(string: str, attributes: [NSAttributedStringKey.foregroundColor:UIColor.white])
+
+    }
+    
+    //
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    //Set data in pickerview
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return DataManager.sharedManager.nameData[row]
     }
     
+    //Set number of rows
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return DataManager.sharedManager.nameData.count
     }
     
+    //When item is selected in pickerview do...
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         temp = DataManager.sharedManager.nameData[row]
     }
     
-    @IBAction func donePressed(_ sender: UIButton) {
+    
+    @IBAction func addPressed(_ sender: UIButton) {
         DataManager.sharedManager.moneyData[DataManager.sharedManager.nameData.index(of: temp)!] = Int(totalMoney.text!)!
     }
     
